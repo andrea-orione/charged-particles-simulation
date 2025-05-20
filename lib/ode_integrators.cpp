440301d9b0c9762f9a0756c182cba498b7f33eaf
Andrea Orione <andrea.orione02@gmail.com>
2025-05-20 15:45:40 +0200

    Changed to dynamic arrays

--- lib/ode_integrators.cpp
+++ lib/ode_integrators.cpp
@@ -1,54 +1,33 @@
 #include "ode_integrators.h"
 
-#include <stdexcept>
-
 #define MAX_NVARS 256
 
-void RK2_step(const double t, double *const y, Rhs_f rhs_f, const double dt,
-              const int nVars) {
-  // Preliminary check
-  if (nVars > MAX_NVARS)
-    throw std::invalid_argument(
-        "rungeKutta2Step: nVars too high. Maximum supported is 256");
+void RK4_step(const double t, double *y, double *dydt, Rhs_f rhs_f,
+              const double dt, const int nVars) {
+  double *yTemp = new double[nVars]; // this one gets reused
+  double *k1 = new double[nVars];
+  double *k2 = new double[nVars];
+  double *k3 = new double[nVars];
 
-  // Reuse the k to save space
-  double y1[MAX_NVARS], k[MAX_NVARS];
-
-  rhs_f(t, y, k);
+  rhs_f(t, y, dydt);
   int i;
   for (i = 0; i < nVars; i++)
-    y1[i] = y[i] + 0.5 * dt * k[i];
+    yTemp[i] = y[i] + 0.5 * dt * dydt[i];
 
-  rhs_f(t + 0.5 * dt, y1, k);
-  for (i = 0; i < nVars; i++)
-    y[i] += dt * k[i];
-}
-
-void RK4_step(const double t, double *y, Rhs_f rhs_f, const double dt,
-              const int nVars) {
-  // Preliminary check
-  if (nVars > MAX_NVARS)
-    throw std::invalid_argument(
-        "rungeKutta4Step: nVars too high. Maximum supported is 256");
-
-  // Reuse the ytemp to save space
-  double yTemp[MAX_NVARS];
-  double k1[MAX_NVARS], k2[MAX_NVARS], k3[MAX_NVARS], k4[MAX_NVARS];
-
-  rhs_f(t, y, k1);
-  int i;
+  rhs_f(t + 0.5 * dt, yTemp, k1);
   for (i = 0; i < nVars; i++)
     yTemp[i] = y[i] + 0.5 * dt * k1[i];
 
   rhs_f(t + 0.5 * dt, yTemp, k2);
   for (i = 0; i < nVars; i++)
-    yTemp[i] = y[i] + 0.5 * dt * k2[i];
+    yTemp[i] = y[i] + dt * k2[i];
 
-  rhs_f(t + 0.5 * dt, yTemp, k3);
+  rhs_f(t + dt, yTemp, k3);
   for (i = 0; i < nVars; i++)
-    yTemp[i] = y[i] + dt * k3[i];
+    y[i] += dt * (dydt[i] + k3[i] + 2. * (k1[i] + k2[i])) / 6.;
 
-  rhs_f(t + dt, yTemp, k4);
-  for (i = 0; i < nVars; i++)
-    y[i] += dt * (k1[i] + k4[i] + 2. * (k2[i] + k3[i])) / 6.;
+  delete[] yTemp;
+  delete[] k1;
+  delete[] k2;
+  delete[] k3;
 }
--- lib/ode_integrators.h
+++ lib/ode_integrators.h
@@ -13,26 +13,28 @@ typedef void (*Rhs_f)(double, double *, double *);
 
 /**
  * Calculate the solution of the ode after a time-step using Runge-Kutta
- * midpoint of order 2 method.
+ * simpson of order 4 method.
  *
  * Given an ordinary differential equation of the form
  * dy/dt = rhs(y,t)
- * and given the solution y(t) at a certain time, this returns the solution
- * y(t+dt) after a time-step dt.
- * This is done evaluating rhs twice.
- * The error goes with O(dt^3).
+ * and given the solution y(t) and its derivative dy/dt at a certain time,
+ * this returns the solution y(t+dt) after a time-step dt.
+ * This is done evaluating rhs three times.
+ * The error goes with O(dt^5).
+ * NOTE: The algorithm takes the derivative at the starting point so
+ * if you need to run the step multiple times varying the timestep, the starting
+ * derivative doesn't get recomputed needlessly.
  *
  * @param t The time at which the step starts.
  * @param y [in/out] An array containing the starting state.
  * It will also be used to store the final state.
+ * @param dydt An array containing the derivative of the starting state.
  * @param rhs_f The right hand side of the differential equation.
  * @param dt The step of the increment.
  * @param nVars The number of equations in the system.
- * Maximum number supported is 256.
- *
- * @throws std::invalid_argument if nVars exceeds the supported value
  */
-void RK2_step(double t, double *y, Rhs_f rhs_f, double dt, int nVars);
+void RK4_step(double t, double *y, double *dydt, Rhs_f rhs_f, double dt,
+              int nVars);
 
 /**
  * Calculate the solution of the ode after a time-step using Runge-Kutta
@@ -40,14 +42,18 @@ void RK2_step(double t, double *y, Rhs_f rhs_f, double dt, int nVars);
  *
  * Given an ordinary differential equation of the form
  * dy/dt = rhs(y,t)
- * and given the solution y(t) at a certain time, this returns the solution
- * y(t+dt) after a time-step dt.
- * This is done evaluating rhs four times.
+ * and given the solution y(t) and its derivative dy/dt at a certain time,
+ * this returns the solution y(t+dt) after a time-step dt.
+ * This is done evaluating rhs three times.
  * The error goes with O(dt^5).
+ * NOTE: The algorithm takes the derivative at the starting point so
+ * if you need to run the step multiple times varying the timestep, the starting
+ * derivative doesn't get recomputed needlessly.
  *
  * @param t The time at which the step starts.
  * @param y [in/out] An array containing the starting state.
  * It will also be used to store the final state.
+ * @param dydt An array containing the derivative of the starting state.
  * @param rhs_f The right hand side of the differential equation.
  * @param dt The step of the increment.
  * @param nVars The number of equations in the system.
@@ -55,6 +61,6 @@ void RK2_step(double t, double *y, Rhs_f rhs_f, double dt, int nVars);
  *
  * @throws std::invalid_argument if nVars exceeds the supported value
  */
-void RK4_step(double t, double *y, Rhs_f rhs_f, double dt, int nVars);
+void adaptive_RK4_step(double t, double *y, Rhs_f rhs_f, int nVars);
 
 #endif
